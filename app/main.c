60251c716717ff2035bbd6493786a8860274ed45
ehotez <egor2013.ep@gmail.com>
2023-09-26 13:24:15 +0300

    hm

--- app/main.c
+++ app/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-//#include "myfunc.h"
+#include "myfunc.h"
 
 int main()
 {
     printf("Hello World!\n");
-//    double x1;
-//    double x2; //roots
-//    quad_equation(2, 1, -3, &x1, &x2);
-//    printf("Roots: %lf and %lf\n", x1, x2);
+    double x1;
+    double x2; //roots
+    quad_equation(2, 1, -3, &x1, &x2);
+    printf("Roots: %lf and %lf\n", x1, x2);
     return 1;
 }
