e769bc77497ad88c77045e2c76283be78827f0c0
ehotez <egor2013.ep@gmail.com>
2023-09-26 10:48:04 +0300

    coveralls check

--- app/main.c
+++ app/main.c
@@ -5,7 +5,7 @@
 int main()
 {
     printf("Hello World!\n");
-    double x1, x2;
+    double x1, x2; //roots
     quad_equation(2, 1, -3, &x1, &x2);
     printf("Roots: %lf and %lf\n", x1, x2);
     return 1;
