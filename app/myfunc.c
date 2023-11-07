73735b0614cc97fd9bb1736148f77c3e50f6fb10
ehotez <egor2013.ep@gmail.com>
2023-11-07 10:29:25 +0300

    root count

--- app/myfunc.c
+++ app/myfunc.c
@@ -4,15 +4,19 @@
 #include <limits.h>
 #include "myfunc.h"
 
-void quad_equation(double a, double b, double c, double *x1, double *x2) {
+int quad_equation(double a, double b, double c, double *x1, double *x2) {
     double discriminant = b * b - 4 * a * c;
+    int count = 0;
     if (discriminant < 0 || a == 0) {
-        *x1 = *x2 = INT_MAX;
+        *x1 = *x2 = 0;
     } else if (discriminant == 0) {
         *x1 = *x2 = -b / (2 * a);
+        count = 1;
     } else {
         *x1 = (-b - sqrt(discriminant)) / (2 * a);
         *x2 = (-b + sqrt(discriminant)) / (2 * a);
+        count = 2;
     }
+    return count;
 }
 
--- tests/quad_equation_test.h
+++ tests/quad_equation_test.h
@@ -14,49 +14,50 @@ extern "C" {
 
 TEST(QuadEquationTest, TwoRealRoots) {
     double a = 1.0, b = -3.0, c = 2.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 2);
     ASSERT_EQ(x1, 1.0);
     ASSERT_EQ(x2, 2.0);
 }
 
 TEST(QuadEquationTest, OneRealRoot) {
     double a = 1.0, b = -4.0, c = 4.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 1);
     ASSERT_EQ(x1, 2.0);
     ASSERT_EQ(x2, 2.0);
 }
 
 TEST(QuadEquationTest, NoRealRoots) {
     double a = 1.0, b = 2.0, c = 3.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
-    ASSERT_EQ(x1, INT_MAX);
-    ASSERT_EQ(x2, INT_MAX);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 0);
 }
 
 TEST(QuadEquationTest, NegativeA) {
     double a = -1.0, b = 2.0, c = 3.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 2);
     ASSERT_EQ(x1, 3.0);
     ASSERT_EQ(x2, -1.0);
 }
 
 TEST(QuadEquationTest, ZeroA) {
     double a = 0.0, b = 2.0, c = 3.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
-    ASSERT_EQ(x1, INT_MAX);
-    ASSERT_EQ(x2, INT_MAX);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 0);
 }
 
 TEST(QuadEquationTest, ZeroB) {
     double a = 1.0, b = 0.0, c = 3.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
-    ASSERT_EQ(x1, INT_MAX);
-    ASSERT_EQ(x2, INT_MAX);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 0);
 }
 
 TEST(QuadEquationTest, ZeroC) {
     double a = 1.0, b = 2.0, c = 0.0, x1 = 0.0, x2 = 0.0;
-    quad_equation(a, b, c, &x1, &x2);
+    int count = quad_equation(a, b, c, &x1, &x2);
+    ASSERT_EQ(count, 2);
     ASSERT_EQ(x1, -2.0);
     ASSERT_EQ(x2, 0.0);
 }
